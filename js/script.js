7c6872e8834c4b8989f1bc80ea558ae6e97539a8
federicaarosio <arosio.fede@gmail.com>
2023-11-14 14:39:01 +0100

    added comments with steps

---
+++ js/script.js
@@ -0,0 +1,9 @@
+// All'interno ciclo for scrivo numeri da 1 a 100
+
+    //Se
+        //il numero / 3 dà resto 0
+            // allora stampo in console "Fizz"
+        //il numero / 5 dà resto 0
+            // allora stampo in console "Buzz"
+        // il numero è sia /3 e /5 con resto 0 (uso un &&)
+            //allora stampo in console "FizzBuzz"
