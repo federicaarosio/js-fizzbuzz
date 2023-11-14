16eade9d0b0382ebc306450b1171b984ea453a9e
federicaarosio <arosio.fede@gmail.com>
2023-11-14 16:08:07 +0100

    added css, classes for different output

--- js/script.js
+++ js/script.js
@@ -2,7 +2,6 @@ const wrapperElement = document.querySelector("div.wrapper");
 console.log(wrapperElement);
 
 
-
 for(let i = 1; i < 101; i++){
 
     if (i % 3 == 0 && i % 5 == 0) {
@@ -15,16 +14,20 @@ for(let i = 1; i < 101; i++){
         console.log(i);
     }
 
+
     const rowElement = document.createElement("div");
-    rowElement.className += " row";
     
     if (i % 3 == 0 && i % 5 == 0) {
+        rowElement.className = " row-FizzBuzz";
         rowElement.innerHTML = ("FizzBuzz");
     } else if (i % 3 == 0) {
+        rowElement.className = " row-Fizz";
         rowElement.innerHTML = ("Fizz");
     } else if (i % 5 == 0) {
+        rowElement.className = " row-Buzz";
         rowElement.innerHTML = ("Buzz");
     } else {
+        rowElement.className = " row-number";
         rowElement.innerHTML = (i);
     }
 
